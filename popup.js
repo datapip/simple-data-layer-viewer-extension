dff3de66767d82873a94c9f6fd184a3ecd469d49
Pip <pip@Pips-Air.fritz.box>
2021-01-18 21:55:56 +0100

    added collapse button

--- popup.js
+++ popup.js
@@ -19,7 +19,7 @@ function getAndDisplayDataLayer() {
 							'<h2 id="headline">'+response.data[0][0]+'</h2>\
 							<p id="details"><i id="location"><b>on </b>'+response.url+'</i><br><i id="time"><b>at </b>'+now.toLocaleString(undefined, { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' })+'</i></p>\
 							<div id="selector"></div>\
-							<div id="clipboard"><button id="copytoclipboard">Copy to Clipboard</button></div>\
+							<div id="buttons"><div id="collapseall">Collapse all</div><div id="expandall" style="display: none">Expand all</div><div id="copytoclipboard">Copy to clipboard</div></div>\
 							<div id="data"></div>\
 							<div id="version">'+version+'</div>';
 						
@@ -40,6 +40,37 @@ function getAndDisplayDataLayer() {
 							document.execCommand("copy");
 							document.body.removeChild(copyText);
 						}
+
+						var collapseButton = document.getElementById('collapseall');
+						collapseButton.addEventListener('click', function() {handleSyntax('collapse')});
+
+						var expandButton = document.getElementById('expandall');
+						expandButton.addEventListener('click', function() {handleSyntax('expand')});
+
+						function handleSyntax(task) {
+							console.log(this.id);
+							console.log(response.data);
+							if(task==="expand") {
+								expandButton.style.display = "none";
+								collapseButton.style.display = "inline-block";
+								dataContainer.innerHTML = "";
+								dataContainer.appendChild(
+									renderjson//.set_icons('+', '-')
+									.set_show_to_level("all")(JSON.parse(response.data[index][1]))
+								);
+								document.getElementById("headline").innerHTML = response.data[index][0];
+							}
+							if(task==="collapse") {
+								expandButton.style.display = "inline-block";
+								collapseButton.style.display = "none";
+								dataContainer.innerHTML = "";
+								dataContainer.appendChild(
+									renderjson//.set_icons('+', '-')
+									.set_show_to_level("1")(JSON.parse(response.data[index][1]))
+								);
+								document.getElementById("headline").innerHTML = response.data[index][0];
+							}
+						}
 						
 						if(response.data.length > 1) {
 							for(var i=0; i<response.data.length; i++) {
@@ -53,6 +84,8 @@ function getAndDisplayDataLayer() {
 
 							function changeObject() {
 								index = parseInt(this.id);
+								expandButton.style.display = "none";
+								collapseButton.style.display = "inline-block";
 								for(var i=0; i < response.data.length; i++ ) {
 									if(index != i) {
 										document.getElementById(i).className = "";
